95fb1e25ad1239fdc9dbb2756c3b0bb16e3f18c7
Lipedev300 <luizfelipebasso22@gmail.com>
2025-10-12 23:20:45 -0300

    Terminadas as entidades Produto e Usuário. Elas ainda são classes puras em java e ainda não possuem anotações de ID ou outros relacionamentos para conexão com banco de dados. Já estão gerados os métodos geters e seters

--- src/main/java/com/lipe/gerenciador_estoque/Entity/Produto.java
+++ src/main/java/com/lipe/gerenciador_estoque/Entity/Produto.java
@@ -1,6 +1,94 @@
 package com.lipe.gerenciador_estoque.Entity;
 
 public class Produto {
+    private int id;
     private String nome;
     private int quantidade;
+    private int categoria;
+    private String modelo;
+    private String sistema_operacional;
+    private String processador;
+    private String memoria_ram;
+    private String armazenamento;
+    private double preco;
+    private int id_usuario;
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getSistema_operacional() {
+        return sistema_operacional;
+    }
+
+    public void setSistema_operacional(String sistema_operacional) {
+        this.sistema_operacional = sistema_operacional;
+    }
+
+    public String getProcessador() {
+        return processador;
+    }
+
+    public void setProcessador(String processador) {
+        this.processador = processador;
+    }
+
+    public String getMemoria_ram() {
+        return memoria_ram;
+    }
+
+    public void setMemoria_ram(String memoria_ram) {
+        this.memoria_ram = memoria_ram;
+    }
+
+    public String getArmazenamento() {
+        return armazenamento;
+    }
+
+    public void setArmazenamento(String armazenamento) {
+        this.armazenamento = armazenamento;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }      
 }
